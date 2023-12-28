cf7f43fa37e992f174c40f41420d5320d9ae219f
Bogusz Mędykowski <bogusz.medykowski@gmail.com>
2023-12-28 13:25:05 +0100

    try send email

--- www/index.php
+++ www/index.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Life without mouse</title>
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
+
 <body>
 
     <header>
@@ -22,7 +24,8 @@
         <h2>About me</h2>
         <p>I am a computer science student who loves to procrastinate with tools for productivity.</p>
         <ul>
-            <li><a href="https://app.skillsclub.com/credential/37427-a1ab86ea0c99b056bbd0eaef2be1c772db012b9960a27c56290a1dfb77a0c7e2" target="_blank">ISTQB certificate</a></li>
+            <li><a href="https://app.skillsclub.com/credential/37427-a1ab86ea0c99b056bbd0eaef2be1c772db012b9960a27c56290a1dfb77a0c7e2"
+                    target="_blank">ISTQB certificate</a></li>
             <li><a href="https://github.com/boguszmedykowski" target="_blank">GitHub</a></li>
             <li><a href="https://pl.linkedin.com/in/bogusz-m%C4%99dykowski-15b82b117" target="_blank">Linkedin</a></li>
         </ul>
@@ -43,7 +46,24 @@
         <ul>
             <li><a href="https://inzynierka.fly.dev/login" target="_blank">Flet web app</a></li>
         </ul>
+
+        <section>
+            <!-- Existing content -->
+
+            <h2>Contact Me</h2>
+            <form action="send_email.php" method="post">
+                <label for="name">Name:</label><br>
+                <input type="text" id="name" name="name" required><br>
+                <label for="email">Email:</label><br>
+                <input type="email" id="email" name="email" required><br>
+                <label for="message">Message:</label><br>
+                <textarea id="message" name="message" required></textarea><br>
+                <input type="submit" value="Send">
+            </form>
+        </section>
+
     </section>
 
 </body>
+
 </html>
